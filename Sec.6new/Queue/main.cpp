62310110a1eb97707f56889681f1413edb8f59b6
Mariom-colab <essmatmariem@gmail.com>
2025-04-02 18:22:59 +0200

    Update main.cpp

--- Sec.6new/Queue/main.cpp
+++ Sec.6new/Queue/main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include "Queue.h"
 
+using namespace std;
+
+int main() {
+  Queue<int> q;
+  
+  // test empty and full
+  q.enqueue(1);
+  q.enqueue(2);
+  q.enqueue(3);
+  q.enqueue(4);
+  q.enqueue(5);
+  q.enqueue(6);
+  q.print();
+
+  // test dequeue
+  q.dequeue();
+  q.print();
+
+  // test dequeue and empty
+  q.dequeue();
+  q.dequeue();
+  q.dequeue();
+  q.dequeue();
+  q.dequeue();
+  q.dequeue();
+  q.print();
+
+  // test circular behaviour
+  for (int i = 0; i < 10; i++) {
+    q.enqueue(i);
+    q.print();
+
+    q.dequeue();
+  }
+
+  return 0;
+}
