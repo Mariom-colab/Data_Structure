e3b492c0cff6c334a79da32f35b5f2c37e4a5ea6
Mariom-colab <essmatmariem@gmail.com>
2025-04-07 00:49:47 +0200

    Update main.cpp

--- Sec.6new/Stack/main.cpp
+++ Sec.6new/Stack/main.cpp
@@ -1,19 +1,72 @@
 #include "Stack.h"
 #include <iostream>
 
-int main() {
-  Stack<int> st;
+template <typename t>
+ Stack<t>::Stack(s)
+ {
+     arr= new t[s];
+    top_index=-1;
 
-  std::cout << st.isEmpty() << std::endl;
+}
 
-  st.push(1);
-  st.push(2);
-  st.print();
+template <typename t>
+ Stack<t>::~Stack()
+ {
+     delete[] arr;
 
-  st.pop();
-  std::cout << st.top() << std::endl;
+}
+template <typename t>
+bool Stack<t>::isEmpty() const {
+    return top_index==-1;
 
-  st.print();
+}
 
-  return 0;
+template <typename t>
+bool Stack<t>::isFull() const {
+
+    return top_index==9;
+
+}
+
+template <typename t>
+void Stack<t>::push(t data) {
+    if(isFull())
+    {
+        std::cout<<"My apologies, your Stack is Full"<<std::endl;
+    }
+    else
+    arr[++top_index]=data;
+
+}
+
+template <typename t>
+void Stack<t>::pop() {
+
+    if(isEmpty())
+    {
+        std::cout<<"My apologies, your Stack is Empty"<<std::endl;
+    }
+    else
+    top_index--;
+
+}
+
+template <typename t>
+int Stack<t>::top() const {
+    if(isEmpty())
+    {
+        std::cout<<"My apologies, your Stack is Empty"<<std::endl;
+    }
+    else
+
+    return arr[top_index];
+
+}
+
+template <typename t>
+void Stack<t>::print() const {
+  for (int i = 0; i <= top_index; i++) {
+    std::cout << arr[i] << ' ';
+  }
+  std::cout << std::endl;
 }
