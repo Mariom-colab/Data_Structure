2f5d614d362a8567c1d5cb4e199612ffe8e09231
Mariom-colab <essmatmariem@gmail.com>
2025-04-24 05:01:15 +0200

    Create LinkedList.h

---
+++ Sec.7/Linked List/LinkedList.h
@@ -0,0 +1,155 @@
+#pragma once
+#include <iostream>
+
+using std::ostream;
+
+template <typename T>
+struct Node {
+  T data;
+  Node *next;
+};
+
+template <typename T>
+class LinkedList {
+private:
+  Node<T> *head;
+  int size;
+
+  // always remeber to delete unused Node
+public:
+  // assign the head to null
+  LinkedList() { head=NULL;
+  size=0;
+
+  }
+
+  // delete all nodes in the list
+  ~LinkedList() {
+      while(head!=NULL)
+      {
+              Node<T> *temp=head;
+              head=head->next;
+              delete temp;
+
+
+      }
+
+  }
+
+  // for debugging
+  friend ostream& operator<<(ostream& os, const LinkedList<T>& list) {
+
+  }
+
+  // insert a new node at the given index
+  void insertAt(int index, const T& data) {
+    if (index < 0 || index > size) return;
+
+    Node<T>* newcar = new Node<T>();
+    newcar->data = data;
+
+    if (index == 0) {
+        newcar->next = head;
+        head = newcar;
+    } else {
+        Node<T>* temp = head;
+        for (int i = 0; i < index ; ++i) {
+            temp = temp->next;
+        }
+        newcar->next = temp->next;
+        temp->next = newcar;
+    }
+
+    size++;
+}
+
+  // remove the node at the given index
+  void removeAt(int index) {
+
+        Node<T> *temp1=head;
+        Node<T> *temp2=head;
+
+        for(int i=0;i<index;i++)
+                  {
+                      temp1=temp1->next;
+                      temp2=temp2->next;
+
+                  }
+                  temp1=temp1->next;
+                  delete temp2;
+
+
+
+  }
+
+  // return the node at the given index
+  T operator[](int index) {
+
+      T* E;
+      E=head;
+      for(int i=0;i<index;i++)
+      {
+          E=E->next;
+      }
+      return E->data;
+
+  }
+
+
+  // insert a new node at the end of the list
+  void insertLast(const T& data) {
+      Node* E;
+      E=head;
+
+      Node<T>* newcar = new Node<T>();
+    newcar->data = data;
+
+      while(E!=NULL)
+      {
+          E=E->next;
+      }
+      E->next=newcar;
+      newcar->next=NULL;
+      size++;
+
+  }
+  // remove the last node
+  void removeLast() {
+      if(size==0)throw"Empty!!";
+
+    Node* E,*F;
+      E=head;
+      F=E->next;
+      while(F->next==NULL)
+      {
+          E=E->next;
+          F=F->next;
+      }
+      ~LinkedList F;
+      E->next=NULL;
+
+    size--;
+  }
+
+
+  // insert a new node at the beginning of the list
+  void insertFirst(const T& data) {
+      Node<T>* newcar=new Node<T>;
+      newcar->data=data;
+      newcr->next=head->next;
+      head=newcar;
+
+  }
+
+  // remove the first node
+  void removeFirst() {
+        Node<T> *temp;
+        temp=head;
+        head=head->next;
+
+        ~LinkedList temp;
+
+
+
+  }
+};
